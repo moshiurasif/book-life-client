710b3a53682bc32be0c40a706a2afa8bdb13a697
asif2212 <asifmd2212@gamil.com>
2021-04-17 15:32:26 +0600

    done

---
+++ src/Components/firebase.config/firebase.config.js
@@ -0,0 +1,9 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyBwzY9mDEG2glRCywtuFn8xrI88uJzYh64",
+    authDomain: "book-life-bd.firebaseapp.com",
+    projectId: "book-life-bd",
+    storageBucket: "book-life-bd.appspot.com",
+    messagingSenderId: "114667059495",
+    appId: "1:114667059495:web:0e31a450d3dde672cc9102"
+};
+export default firebaseConfig;
